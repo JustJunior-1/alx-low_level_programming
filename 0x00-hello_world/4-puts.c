e72efded7abfce36f11f89c5291704437073c398
Kwasi Appiah <appiahkwasiampem@gmail.com>
2022-09-08 05:48:41 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+nes (10 sloc)  201 Bytes
+
+
+
+#include <stdio.h>
+
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
