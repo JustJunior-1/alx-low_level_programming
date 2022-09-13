cbe6214e9fe0b2c10ff199a4e493d69e5fb8e8fd
JustJunior-1 <108819844+JustJunior-1@users.noreply.github.com>
2022-09-13 11:53:21 +0000

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,12 @@
-#include "holberton.h"
-
+#include "JustJunior-1.h"
 /**
- *
- *  * _abs - calculate the absolute value of the number from zero.
- *
- *   * @i: the integer to be converted.
- *
- *    * Return: absolute value of the input number.
- *
- *     */
-
+* _abs - calculate the absolute value of the number from zero.
+* @i: the integer to be converted.
+* Return: absolute value of the input number.
+*/
 int _abs(int i)
-
 {
-
-		if (i < 0)
-
-					i = i * -1;
-
-			return (i);
-
+if (i < 0)
+i = i * -1;
+return (i);
 }
